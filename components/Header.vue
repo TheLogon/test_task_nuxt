b0137b67ab9f032ceaebd7be3488ce6bab24473e
TheLogon <kirilld539@gmail.com>
2023-01-10 11:39:38 +0500

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <div class="header__container">
+      <div class="header__inner">
+        <div class="header__logo">
+          <img src="@/assets/img/logo.svg" alt="" />
+        </div>
+        <div class="header__menu">
+          <a href="#" class="header__menu_item active">About</a>
+          <a href="#" class="header__menu_item">Map</a>
+          <a href="#" class="header__menu_item">Zoos</a>
+          <a href="#" class="header__menu_item">Donate</a>
+          <a href="#" class="header__menu_item">Contact us</a>
+        </div>
+        <p class="header__copyright">Designed by ©</p>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss"></style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Header />
+    <section class="intro">
+      <div class="intro__container">
+        <div class="intro__inner">
+          <div class="intro__offer">
+            <img src="@/assets/img/ellipse-offer.png" class="ellipse" alt="" />
+            <h1>Watch your <span>favorite</span> animal online</h1>
+          </div>
+          <a href="#" class="intro__btn btn">watсh online</a>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/style.scss";
+</style>
